6ca35c2957d27d8566c116d4462cdeb49e0a7acb
jess <jessica.elessa@gmail.com>
2022-02-11 06:20:21 +0100

    fixed old conflicts and deleted footer margin

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 // MENU BURGER
-function toggleMenu () {  
-    const navbar = document.querySelector(".navbar");
-    const burger = document.querySelector(".burger");
-    burger.addEventListener("click", (e) => {    
-      navbar.classList.toggle("show-nav");});  
+function toggleMenu() {
+  const navbar = document.querySelector(".navbar");
+  const burger = document.querySelector(".burger");
+  burger.addEventListener("click", (e) => {
+    navbar.classList.toggle("show-nav");
+  });
 }
 
 toggleMenu();
-  
+
 // BOUTON ASCENSEUR
 
 //On crée une variable bouton
 button = document.getElementById("button-top");
 
-// scrollTop = distance entre le haut de l'élément et la partie la plus haute de son contenu visible. 
+// scrollTop = distance entre le haut de l'élément et la partie la plus haute de son contenu visible.
 
 // Si distance de plus de 500px depuis le haut du body, alors on fait apparaître le bouton
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) 
-  {button.style.display = "block";} 
- 
-  else {button.style.display = "none";}
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
 }
 
-scrollFunction ();
+scrollFunction();
 
 // scrollTop de l'élément = 0 => retour au début du contenu de l'élément (ici, c'est le body = haut de la page)
 function topFunction() {
@@ -33,4 +40,4 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // Pour Chrome, Firefox, IE and Opera
 }
 
-topFunction ();
+topFunction();
